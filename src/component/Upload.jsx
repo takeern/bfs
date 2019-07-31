f1437d57ae83efd1e7c01e66f6975458b6297543
qu tang <takeern@gmail.com>
2019-07-31 17:27:00 +0800

    [update] api

--- src/component/Upload.jsx
+++ src/component/Upload.jsx
@@ -1,39 +1,61 @@
 import React, { useRef } from 'react';
 import { API } from '../plugins/API';
 
+const toast = (text, timeout = 3000) => {
+    const div = document.createElement('div');
+    div.className = 'toast';
+    div.innerText = text;
+    document.body.appendChild(div);
+    setTimeout(() => {
+        div.style.opacity = 0;
+        setTimeout(() => {
+            document.body.removeChild(div);
+        }, 3000)
+    }, timeout - 3000);
+}
+
 export default () => {
     const inputEl = useRef(null);
     const fileNameWrapper = useRef(null);
     const pathWrapper = useRef(null);
+    const selectType = useRef(null);
+    const mask = useRef(null);
     const handleClick = () => {
         inputEl.current.click();
     };
-    const handleSumbit = (files) => {
-        const fd = new FormData();
-        fd.append('file', files[0]);
-        fd.append('ts', Date.now());
-        fetch(API.UPLOAD_PATH, {
-            method: 'Post',
-            body: fd,
-            mode: 'cors',
-        }).then(res => res.json && res.json())
-        .then(res => {
+    const handleSubmit = async (files) => {
+        const journalType = selectType.current.value;
+        if (journalType === 'none') {
+            return toast('请选择上传期刊', 5000);
+        }
+        for (let i = 0; i < files.length; i ++) {
+            const fd = new FormData();
+            fd.append('file', files[i]);
+            fd.append('ts', Date.now());
+            fd.append('journalType', journalType);
+            const res = await fetch(API.UPLOAD_PATH, {
+                method: 'Post',
+                body: fd,
+                mode: 'cors',
+            }).then(res => res.json && res.json());
             if (res.code === 1) {
                 const nameDiv = document.createElement('div');
                 const pathDiv = document.createElement('div');
                 pathDiv.innerText = res.path;
-                nameDiv.innerText = files[0].name;
+                nameDiv.innerText = files[i].name;
                 fileNameWrapper.current.appendChild(nameDiv);
                 pathWrapper.current.appendChild(pathDiv);
+            } else {
+                res.msg ? toast(res.msg) : toast('未知错误');
             }
-        });
+        }
     };
     const handleOverDrag = (e) => {
         e.stopPropagation(); 
         e.preventDefault(); 
     };
     const handleDrop = (e) => {
-        handleSumbit(e.dataTransfer.files);
+        handleSubmit(e.dataTransfer.files);
         handleOverDrag(e);
     };
     return (
@@ -59,7 +81,7 @@ export default () => {
                 <input type='file' 
                 multiple 
                 ref={inputEl}
-                onChange={(e) => handleSumbit(e.target.files)}
+                onChange={(e) => handleSubmit(e.target.files)}
                 style={{
                     display: 'none',
                 }}/>
@@ -79,6 +101,23 @@ export default () => {
                 }}
                 onClick={(e) => handleClick(e)}
                 >上传文件</button>
+                <select
+                ref={selectType}
+                style={{
+                    display: 'block',
+                    margin: '0 auto',
+                    background: '#fafdfe',
+                    height: '28px',
+                    width: '180px',
+                    lineHeight: '28px',
+                    border: '1px solid #9bc0dd',
+                    borderRadius: '2px',
+                }}>
+                    <option value='none'>选择上传期刊</option>
+                    <option value='JISSR'>JISSR</option>
+                    <option value='IJPEE'>IJPEE</option>
+                    <option value='bryanhousepub'>bryanhousepub</option>
+                </select>
             </div>
             <div style={{
                 width: '600px',
--- src/route.js
+++ src/route.js
@@ -3,15 +3,10 @@ import Upload from './component/Upload';
 
 const routes = [
     {
-        path: '/',
-        component: Home,
-        exact: true,
-    },
-    {
         path: '/upload',
         component: Upload,
         exact: true,
-    }
+    },
 ];
 
 export default routes;

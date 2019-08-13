de2b63342e2f29b2b110b2379c0233f42fb4f82d
takeern <tq123456@foxmail.com>
2019-08-14 01:06:43 +0800

    '[update]'

--- src/route.js
+++ src/route.js
@@ -1,10 +1,21 @@
-import Home from './component/Home';
+import Signin from './component/Signin';
+import SignUp from './component/signUp';
 import Upload from './component/Upload';
 
 const routes = [
     {
         path: '/upload',
         component: Upload,
+        exact: true,
+    },
+    {
+        path: '/signIn',
+        component: Signin,
+        exact: true,
+    },
+    {
+        path: '/signUp',
+        component: SignUp,
         exact: true,
     },
 ];
